34a74272a4b1c209b3a642b610a7162130a08a38
Mirejai <122158782+Mirejai@users.noreply.github.com>
2024-08-20 16:59:12 +0200

    Corrections de fautes.

--- 12. Les Boucles For/tables.cpp
+++ 12. Les Boucles For/tables.cpp
@@ -7,14 +7,14 @@
 
 int main() {
 
-	// Ici on met nombre_1 <= 10 car si on mettait < on aurait pas la table de 10
+	// Ici on met nombre_1 <= 10 car si on mettait < on n'aurait pas la table de 10
 	for (int nombre_1 = 0 ; nombre_1 <= 10 ; nombre_1++){
 
 		// On affique quelle table on va calculer
 		std::cout << "Table de " << nombre_1 << " :" << std::endl;
 
 		// On peut imbriquer des boucles dans d'autres
-		for (int nombre_2 = 0 ; nombre_2 <= 10 ; nombre_2++) // Si le code de la boucle ne comprend qu'une instruction on est pas obligé de mettre des accolades.
+		for (int nombre_2 = 0 ; nombre_2 <= 10 ; nombre_2++) // Si le code de la boucle ne comprend qu'une instruction on n'est pas obligé de mettre des accolades.
 			std::cout << nombre_1 << "x" << nombre_2 << " = " << nombre_1 * nombre_2 << std::endl; // On affiche le produit de nombre_1 et nombre_2
 
 		
@@ -23,6 +23,6 @@ int main() {
 
 	}
 
-	return 0; // On oubli pas return 0; bien sûr.
+	return 0; // On n'oubli pas return 0; bien sûr.
 
 }
